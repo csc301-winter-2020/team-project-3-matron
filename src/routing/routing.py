16fc74f08e2be22d5bb10d37f4286a4ce3ccc183
Thomas <th.leung@mail.utoronto.ca>
2020-03-03 23:44:18 -0500

    Changed routing signatures.

--- src/routing/routing.py
+++ src/routing/routing.py
@@ -1,15 +1,13 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
-import LinkedList
+import dao
 import os
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "filler until I know datagbase url"
+app.config["MONGO_URI"] = "mongodb+srv://matron:<password>@matron-db-pxltz.azure.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
-# this will be a dictionary used to store the 
-# pairs of graph and images of a given blueprint
-blueprints = {}
+dao = None
 
 @app.route('/')
 def index():
@@ -17,27 +15,32 @@ def index():
 
 # used to acquire the appropriate blue_print for a
 # specific wing of a specific floor
-@app.route('/blueprint/<string:name>/<date>')
-def get_blueprint(name, date):
+@app.route('/blueprint/<string:name>/request')
+def get_blueprint(name):
     # TODO
     raise NotImplementedError
-    return blueprints[zip(floor_number, wing_number)]
 
-@app.route('/graph/<string:name>/<date>')
-def get_graph(name, date):
+@app.route('/graph/<string:name>/request')
+def get_graph(name):
     # TODO
     raise NotImplementedError    
 
 # TODO add functions to store and save current blue prints
 # to the MongoDB server
-@app.route('/blueprint/<string:name>/<date>', methods=['POST'])
-def saveBlueprint(floor_number, wing_number):
+@app.route('/blueprint/<string:name>/save')
+def saveBlueprint(name):
     raise NotImplementedError
 
 # TODO add functions store and save graph to MongoDB server
-@app.route('/graph/<string:name>/<date>', methods=['POST'])
-def saveGraph(name, date):
+@app.route('/graph/<string:name>/save')
+def saveGraph(name):
+    raise NotImplementedError
+
+# TODO retrieve a list of all versions of a specific graph
+@app.route('/graph/<string:name>/requestAll')
+def get_all_versions(name):
     raise NotImplementedError
 
 if __name__ == '__main__':
     # TODO make this part fetch all items from the server and parse them into 
+    dao = MangoDAO("matron", "<password>")
