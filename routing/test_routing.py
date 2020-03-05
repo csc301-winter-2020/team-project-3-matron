577fcf48e483d51832c63ff5329a37f7a7fc3ff0
Elias Pol <eli.pol@mail.utoronto.ca>
2020-03-05 17:41:25 -0500

    Try moving tests to mix with source -- a little messy

--- routing/test_routing.py
+++ routing/test_routing.py
@@ -1,8 +1,8 @@
 import pytest
 from flask import Flask 
-import sys
-sys.path.append('../routing')
-from routing import app
+#import sys
+#sys.path.append('../routing')
+#from routing import app
 
 
 def test_base_route():
