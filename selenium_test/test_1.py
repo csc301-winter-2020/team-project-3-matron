3b7516403b042f040d07115279324576f6ee4602
JohnPHK <JohnPHK@github.com>
2020-03-08 20:35:21 -0400

    working on selenium nothing much works yet

--- selenium_test/test_1.py
+++ selenium_test/test_1.py
@@ -7,34 +7,58 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 class seleniumtest(unittest.TestCase):
-    
+
+    # def test_Heroku(self):
+        # self.driver = webdriver.Chrome(executable_path="./drivers/chromedriver") 
+        # self.driver.maximize_window()
+        # self.driver.get("https://floating-shore-56001.herokuapp.com/")
+        # time.sleep(1)
+        # print("Title of the page is :" + self.driver.title)
+        # self.driver.close()
+
+    # def test_Bing(self):
+        # self.driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
+        # self.driver.get("https://floating-shore-56001.herokuapp.com/")
+
+        # titleOfWebPage = self.driver.title
+        # self.assertTrue(titleOfWebPage == "Matron Maps")
+        # self.driver.close()
+
+    # def test_get_element(self):
+        # self.driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
+        # self.driver.get("https://floating-shore-56001.herokuapp.com/")
+        # button = self.driver.find_element_by_id('edit_floor')
+        # button.click()
+        # time.sleep(3)
+        # self.driver.close()
 
 
-    def test_Heroku(self):
-        self.driver = webdriver.Chrome(executable_path="../drivers/chromedriver") 
-        self.driver.maximize_window()
+    def test_input_and_click(self):
+        self.driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
+        
         self.driver.get("https://floating-shore-56001.herokuapp.com/")
-        time.sleep(1)
-        print("Title of the page is :" + self.driver.title)
+        inputElement = self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/input[2]")
+        inputElement.send_keys("tests")
+        # button = self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/i")
+        # button.click()
+        time.sleep(10)
+
+
+        # button = self.driver.find_element_by_id('edit_floor')
+        # button.click()
+        # time.sleep(3)
         self.driver.close()
 
-    def test_Bing(self):
-        self.driver = webdriver.Chrome(executable_path="../drivers/chromedriver")
-        self.driver.get("https://floating-shore-56001.herokuapp.com/")
+    def test_left_mouse_click(self):
 
-        titleOfWebPage = self.driver.title
-        self.assertTrue(titleOfWebPage == "Matron Maps")
-
-    def test_get_element(self):
-        self.driver = webdriver.Chrome(executable_path="../drivers/chromedriver")
+        self.driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
         self.driver.get("https://floating-shore-56001.herokuapp.com/")
         button = self.driver.find_element_by_id('edit_floor')
         button.click()
-        time.sleep(5)
-
-
-        
-
+        time.sleep(4)
+        action = webdriver.common.action_chains.ActionChains(self.driver)
+        action.move_to_element_with_offset(button, 10, 10).click().perform()
+        time.sleep(4)
 
 
 
